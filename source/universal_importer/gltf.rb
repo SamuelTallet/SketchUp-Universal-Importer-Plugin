a87b0a763013897d8fc1437cc758f5d0ebdd5e53
Samuel Tallet <samuel.tallet@gmail.com>
2022-08-14 22:28:44 +0200

    Deprecate glTF and OBJ parsers #8

--- source/universal_importer/gltf.rb
+++ source/universal_importer/gltf.rb
@@ -1,24 +1,16 @@
 # Universal Importer extension for SketchUp 2017 or newer.
-# Copyright: © 2019 Samuel Tallet <samuel.tallet arobase gmail.com>
+# Copyright: © 2022 Samuel Tallet <samuel.tallet at gmail dot com>
 # 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3.0 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3.0 of the License, or (at your option) any later version.
 # 
-# If you release a modified version of this program TO THE PUBLIC,
-# the GPL requires you to MAKE THE MODIFIED SOURCE CODE AVAILABLE
-# to the program's users, UNDER THE GPL.
+# If you release a modified version of this program TO THE PUBLIC, the GPL requires you to MAKE THE MODIFIED SOURCE CODE
+# AVAILABLE to the program's users, UNDER THE GPL.
 # 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # 
 # Get a copy of the GPL here: https://www.gnu.org/licenses/gpl.html
-
-raise 'The UIR plugin requires at least Ruby 2.2.0 or SketchUp 2017.'\
-  unless RUBY_VERSION.to_f >= 2.2 # SketchUp 2017 includes Ruby 2.2.4.
 
 require 'fileutils'
 require 'json'
@@ -26,7 +18,7 @@ require 'json'
 # Universal Importer plugin namespace.
 module UniversalImporter
 
-  # Minimal glTF parser.
+  # Minimal glTF parser. @deprecated
   class GlTF
 
     # Parses a glTF file.
