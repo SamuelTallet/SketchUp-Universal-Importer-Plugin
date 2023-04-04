9f8b119913da813f9477b9f350e1b8a1625fcd7f
Samuel Tallet <samuel.tallet@gmail.com>
2023-04-04 14:05:26 +0200

    Bump version to 1.2.2

--- source/universal_importer.rb
+++ source/universal_importer.rb
@@ -21,7 +21,7 @@ raise 'Universal Importer plugin requires at least SketchUp 2017.'\
 # Universal Importer plugin namespace.
 module UniversalImporter
 
-  VERSION = '1.2.1'
+  VERSION = '1.2.2'
 
   # Load translation if it's available for current locale.
   TRANSLATE = LanguageHandler.new('uir.translation')
